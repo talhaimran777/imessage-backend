e69178fdbd231814c2ae496a01ad848e583891f8
Talha Imran <devtal284@gmail.com>
2022-10-08 21:55:40 +0500

    Added mutaion to add a user and store it in a json file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
 const users = require("../data/users");
-
+const fs = require("fs");
 const { ApolloServer, gql } = require("apollo-server-express");
 
 const typeDefs = gql`
@@ -12,15 +13,36 @@ const typeDefs = gql`
   type Query {
     users: [User]
   }
+
+  type Mutation {
+    addUser(name: String, age: Int): User
+  }
 `;
 
 const resolvers = {
   Query: {
     users: () => users,
   },
+  Mutation: {
+    addUser: (_, { name, age }) => {
+      const newUser = { name, age };
+      users.push(newUser);
+
+      fs.writeFile("./data/users.json", JSON.stringify(users), (err) => {
+        if (err) console.log("Error writing file:", err);
+      });
+
+      return newUser;
+    },
+  },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema });
 const app = express();
 
 const PORT = 5000;
